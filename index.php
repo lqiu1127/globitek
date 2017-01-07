faa8fd7240b29b424a8bc12e2fb36171f15f31ab
Tianhao (Leo) Qiu <tiq001@ucsd.edu>
2017-01-07 14:33:25 -0800

    added author tag

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<!--
+    - Globitek Database - Spring 2017 CodePath Web Security Week 1
+    by Tianhao Qiu @UCSD
+    Started: Jan 5th, 2017 
+--> 
 <?php
     require_once ('./public/register.php');
 ?>  
