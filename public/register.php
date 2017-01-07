502f12cf22a3c97298a103d10e7412e0a9abf8a2
Tianhao (Leo) Qiu <tiq001@ucsd.edu>
2017-01-07 13:00:53 -0800

    added error checking and validation

--- public/register.php
+++ public/register.php
@@ -1,41 +1,94 @@
 <?php
-  require_once('../private/initialize.php');
-
+  require_once('/private/initialize.php');
   // Set default values for all variables the page needs.
-
+  $first_name = $last_name = $email = $username = "";
+  $valid = true;
+  $errors = [];
   // if this is a POST request, process the form
-  // Hint: private/functions.php can help
+  // Hint: Write these in private/functions.php
 
-    // Confirm that POST values are present before accessing them.
 
-    // Perform Validations
-    // Hint: Write these in private/validation_functions.php
+    if (is_post_request()) {
+      $errors = [];
+      // Confirm that POST values are present before accessing them.
 
+      // Perform Validations
+      // Hint: Write these in private/validation_functions.php
+
+      //check that first name is valid
+      if (is_blank($_POST['first_name'])) {
+        $errors[] = "First name cannot be blank.";
+        $valid = false;
+      } elseif (!has_length($_POST['first_name'], ['min' => 2, 'max' => 255])) {
+        $errors[] = "First name must be between 2 and 255 characters.";
+        $valid = false;
+      } else {
+        $first_name = trim($_POST['first_name']);
+      }
+
+      // check that late name is valid
+      if (is_blank($_POST['last_name'])) {
+        $errors[] = "Last name cannot be blank.";
+        $valid = false;
+      } elseif (!has_length($_POST['last_name'], ['min' => 2, 'max' => 255])) {
+        $errors[] = "Last name must be between 2 and 255 characters.";
+        $valid = false;
+      } else {
+        $last_name = trim($_POST['last_name']);
+      }
+
+      // check that email is valid
+      if (is_blank($_POST['email'])) {
+        $errors[] = "Email cannot be blank.";
+        $valid = false;
+      } elseif (!has_length($_POST['email'], ['min' => 2, 'max' => 255])) {
+        $errors[] = "Email must be between 2 and 255 characters.";
+        $valid = false;
+      } elseif (!has_valid_email_format($_POST['email'])){
+        $errors[] = "Enter a valid email format.";
+        $valid = false;
+      } else {
+        $email = trim($_POST['email']);
+      }
+
+      //check that username is valid
+      if (is_blank($_POST['username'])) {
+        $errors[] = "Username cannot be blank.";
+        $valid = false;
+      } elseif (!has_length($_POST['username'], ['min' => 8, 'max' => 255])) {
+        $errors[] = "Username must be longer than 8 characters.";
+        $valid = false;
+      } else {
+        $username = trim($_POST['username']);
+      }
+    }
+   
     // if there were no errors, submit data to database
-
+    if ($valid){
       // Write SQL INSERT statement
-      // $sql = "";
+      $sql = sprintf ("INSERT INTO users (first_name, last_name, email, username, created_at)
+      VALUES ('%s','%s','%s','%s','%s'); ", $first_name, $last_name, $email, $username, date("Y-m-d H:i:s"));
 
-      // For INSERT statments, $result is just true/false
-      // $result = db_query($db, $sql);
-      // if($result) {
-      //   db_close($db);
+      //For INSERT statments, $result is just true/false
+      $result = db_query($db, $sql);
+      if($result) {
+        db_close($db);
 
-      //   TODO redirect user to success page
-
-      // } else {
-      //   // The SQL INSERT statement failed.
-      //   // Just show the error, not the form
-      //   echo db_error($db);
-      //   db_close($db);
-      //   exit;
-      // }
-
+        // redirect user to success page
+        header("Location: public/registration_success.php");
+        exit;
+      } else {
+         // The SQL INSERT statement failed.
+         // Just show the error, not the form
+         echo db_error($db);
+         db_close($db);
+         exit;
+      }
+    }
 ?>
 
 <?php $page_title = 'Register'; ?>
 <?php include(SHARED_PATH . '/header.php'); ?>
-
 <div id="main-content">
   <h1>Register</h1>
   <p>Register to become a Globitek Partner.</p>
@@ -43,11 +96,11 @@
   <?php
     // TODO: display any form errors here
     // Hint: private/functions.php can help
-  echo display_errors($errors);
+    echo display_errors($errors);
   ?>
 
   <!-- TODO: HTML form goes here -->
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 
     First Name: 
     <br>
@@ -71,6 +124,12 @@
     -->
     <input type="submit" class="button" name="submit" value="Submit">
   </form>
+  <!-- use java script to reset the page <script>
+    function reload_page() {
+      window.location = "";
+    }
+  </script>
+  -->
 </div>
 
 <?php include(SHARED_PATH . '/footer.php'); ?>
